adca90707328055e1377e51bd61aac7f44f93919
bhavberi <bhavberi@users.noreply.github.com>
2024-12-11 18:01:30 +0000

    Apply Prettier Formatting Fixes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -87,7 +87,7 @@ export default function HomeComponent() {
     else {
       // Get user system preference
       const userPrefersDark = window.matchMedia(
-        "(prefers-color-scheme: dark)"
+        "(prefers-color-scheme: dark)",
       ).matches;
       setDarkMode(userPrefersDark);
     }
@@ -227,10 +227,10 @@ export default function HomeComponent() {
                           width: isMobile
                             ? "90vw"
                             : isTablet
-                            ? "40vw"
-                            : isBigTablet
-                            ? "40vw"
-                            : "15vw",
+                              ? "40vw"
+                              : isBigTablet
+                                ? "40vw"
+                                : "15vw",
                           backgroundColor: theme.palette.background.paper,
                           boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
                           borderRadius: "10px",
@@ -430,10 +430,10 @@ export default function HomeComponent() {
                           width: isMobile
                             ? "90vw"
                             : isTablet
-                            ? "40vw"
-                            : isBigTablet
-                            ? "40vw"
-                            : "15vw", // Responsive width
+                              ? "40vw"
+                              : isBigTablet
+                                ? "40vw"
+                                : "15vw", // Responsive width
                           backgroundColor: theme.palette.background.paper,
                           boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
                           borderRadius: "10px",
