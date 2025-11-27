bc12ead714e871d885deb3919c65e5409eb9eb2b
Bhav Beri <bhavberi@gmail.com>
2025-11-27 18:26:19 +0530

    Update metadata for the site

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,18 @@ import { IntranetAccessProvider } from "@/providers/IntranetAccessProvider";
 export const metadata = {
   title: "Portals@IIITH",
   description: "List of Portals at IIIT Hyderabad",
+  authors: [
+    { name: "Bhav Beri <bhav.beri@research.iiit.ac.in>" },
+    { name: "Shreyansh Bhanu <shreyansh.bhanu@research.iiit.ac.in>" },
+  ],
+  creator: "Bhav Beri",
+  publisher: "Student WebAdmins Team, IIIT Hyderabad",
+  keywords: [
+    "IIIT Hyderabad",
+    "Portals",
+    "Intranet",
+  ],
+  generator: 'Next.js',
 };
 
 export default function RootLayout({ children }) {
